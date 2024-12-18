b070a157b8fefc5bb65c62aa01ee1efdcb2f1cf7
Mathieu Cote <mathieu@effetmonstre.com>
2024-12-18 01:10:32 -0500

    removed component type

--- packages/svelte/src/lib/components/Render.svelte
+++ packages/svelte/src/lib/components/Render.svelte
@@ -1,16 +1,16 @@
 <script module lang="ts">
   import type { PageProps } from '@jamesst20/inertia-core'
-  import type { ComponentType } from 'svelte'
-  
-  import Render from "./Render.svelte"
+  import type { Component } from 'svelte'
+
+  import Render from './Render.svelte'
 
   type RenderProps = {
-    component: ComponentType
+    component: Component
     props?: PageProps
     childComponents?: RenderProps[]
   }
 
-  export const h = (component: ComponentType, props?: PageProps, childComponents?: RenderProps[]): RenderProps => {
+  export const h = (component: Component, props?: PageProps, childComponents?: RenderProps[]): RenderProps => {
     return {
       component,
       ...(props ? { props } : {}),
@@ -24,7 +24,7 @@
 
   let { component, props = {}, childComponents = [] }: RenderProps = $props()
 
-  let prevComponent: ComponentType
+  let prevComponent: Component
   let key: number | null = $state(null)
 
   $effect(() => {
@@ -37,10 +37,10 @@
 
 {#if store.component}
   {#key key}
-    <svelte:component this={component} {...props}>
+    <component {...props}>
       {#each childComponents as child, index (component && component.length === index ? store.key : null)}
         <Render {...child} />
       {/each}
-    </svelte:component>
+    </component>
   {/key}
 {/if}
